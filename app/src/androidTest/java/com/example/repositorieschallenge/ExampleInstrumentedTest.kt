0865b641ca97aa50095b8b3e72b16e524bc0a56f
welli <wellington.terroni@outlookc.om>
2023-04-07 17:36:26 -0300

    Inclui teste instrumentado

--- app/src/androidTest/java/com/example/repositorieschallenge/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/repositorieschallenge/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.repositorieschallenge
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.repositorieschallenge.presentation.RepositoriesActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,20 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get : Rule
+    var mActivityRule = ActivityScenarioRule(RepositoriesActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.repositorieschallenge", appContext.packageName)
+    }
+
+    @Test
+    fun checkScreenLoaded() {
+        onView(withId(R.id.search_layout))
+            .check(matches(isDisplayed()))
     }
 }
